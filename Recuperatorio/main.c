65c41bc416c95affc996d6deb494930dae0ca515
ljcmps01 <ljcmps01@gmail.com>
2022-08-08 19:21:35 -0300

    punto 1 y 2

--- Recuperatorio/main.c
+++ Recuperatorio/main.c
@@ -36,32 +36,35 @@ int nuevoVehiculoParam(eVehiculo *pVehiculo, int id, char* marca, int tipo, floa
 
 int main()
 {
+    int vector=20;
+    printf("%d\n",*(&vector));
 
-    char data[256];
     FILE *archivo;
-
+    char data[256];
     eVehiculo test=nuevoVehiculo();
     nuevoVehiculoParam(&test,1,"Renault",12,12.4);
-/*
-    strcpy(data,"id: ");
-    strcat(data,test.id);
-    strcat(data,"\nMarca: ");
-    strcat(data,test.marca);
-    strcat(data,"\nTipo: ");
-    strcat(data,test.tipo);
-    strcat(data,"\nPeso: ");
-  //  strcat(data,test.peso);
-
-
-    archivo=fopen("data","w");
-    fwrite(archivo,data);
-    fclose(archivo)
-*/
 
     printf("id %d\n",test.id);
     printf("marca %s\n",test.marca);
     printf("tipo %d\n",test.tipo);
     printf("peso %f\n",test.peso);
+
+
+    archivo=fopen("data.txt","w");
+    fprintf(archivo,"id: %d\nmarca: %10s\ntipo: %d\npeso: %.2f\n",test.id,test.marca,test.tipo,test.peso);
+    fclose(archivo);
+
+    archivo=fopen("data.txt","r");
+    fscanf(archivo,"%256s",&data);
+    fclose(archivo);
+
+    printf("%s\n",data);
+
+
+    //archivo=fopen("data.txt","wb");
+    //fwrite();
+    //fclose(archivo);
+
 
     return 0;
 }
